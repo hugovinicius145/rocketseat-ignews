009b432cc7e69730c054d3f498ae3830a045349c
Hugo Vinicius <hugovinicius145@gmail.com>
2021-10-15 15:25:20 -0300

    Static Site Generation SSG.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head'
 import Image from 'next/image'
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -36,7 +36,7 @@ export default function Home({product}: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JcgDpCMyndOZOKuD8Qj5R3L')
 
   const product = {
@@ -44,11 +44,13 @@ export const getServerSideProps: GetServerSideProps = async () => {
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
       currency: 'USD',
-    }).format((price.unit_amount / 100))
+    }).format(price.unit_amount / 100),
   }
+
   return {
     props : {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24,  //24 hours
   }
 }
